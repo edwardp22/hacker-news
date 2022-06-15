9899d8916ac795d6b4c0424037bc18488da67aa4
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-06-15 14:41:22 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hacker News
+
+Shows a list of articles as a result of searching inputed value.
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/TR7gsTb/HACKERNEWS-Thumbnail.png)
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const axios = require("axios");
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+async function searchHN(query) {
+  const response = await axios.get(
+    `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&hitsPerPage=90`
+  );
+
+  return response.data;
+}
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Search Hacker News",
+  });
+});
+
+app.get("/search", async (req, res, next) => {
+  try {
+    const searchQuery = req.query.q;
+    if (!searchQuery) {
+      res.redirect(302, "/");
+      return;
+    }
+
+    const results = await searchHN(searchQuery);
+    res.render("search", {
+      title: `Search results for: ${searchQuery}`,
+      searchResults: results,
+      searchQuery,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  res.set("Content-Type", "text/html");
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.status);
+  res.set("Content-Type", "text/html");
+  res.status(500).send("<h1>Internal Server Error</h1>");
+});
+
+const server = app.listen(port || 3000, () => {
+  console.log(`Hacker news server started on port: ${server.address().port}`);
+});
